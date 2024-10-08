228036c9be4f491d0089a800c01338a6d2a935ca
Alejandro Trinidad <aletriram@gmail.com>
2024-10-08 14:52:51 +0200

    wip componente reloj

---
+++ src/components/TimerHours.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="timer">
+		{{ horas }}:{{ minutos }}:{{ segundos }}
+	</div>
+</template>
+
+<script setup>
+
+import { reactive, computed } from "vue";
+
+const tiempo = reactive(3600 * 3);
+
+const horas = computed(() => {
+	return tiempo % 60;
+})
+
+
+</script>
